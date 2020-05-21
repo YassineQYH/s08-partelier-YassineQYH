15d856591945183f03184e8f6196af578e1d2ea1
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-05-21 15:56:13 +0200

    Etape 1 avec découpage mais les liens vers les articles et catégories envoie sur une erreur

---
+++ wp-content/themes/oblog/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+
+		<!-- Déclaration de notre font -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800" rel="stylesheet">
+
+		<!-- -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+		crossorigin="anonymous">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+		crossorigin="anonymous">
+		<!-- Ma feuille de style pour mon blog -->
+		<link rel="stylesheet" href="/wp-content/themes/oblog/assets/css/blog.css">
+
+		<title>A la dérive, le blog</title>
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+
+	<!-- <div class="body-open"> -->
+		<?php
+			wp_body_open();
+		?>
+	<!-- </div> -->
---
+++ wp-content/themes/oblog/template-parts/header/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Déclaration de notre font -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800" rel="stylesheet">
+
+  <!-- -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+    crossorigin="anonymous">
+  <!-- Ma feuille de style pour mon blog -->
+  <link rel="stylesheet" href="/wp-content/themes/oblog/assets/css/blog.css">
+
+  <title>A la dérive, le blog</title>
+</head>
